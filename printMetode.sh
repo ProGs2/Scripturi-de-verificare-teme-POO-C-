8a34af83e96fdf10e219e069f49ec5b1f10853dd
stanciu.razvan <stanciu.razvan@mta.ro>
2024-06-28 09:08:32 +0300

    final-commit

--- printMetode.sh
+++ printMetode.sh
@@ -3,6 +3,6 @@ for line in $file
 do
 	find=`find ~ -name $line`
 	echo "$find contine metodele:"
-	gasit=$(egrep -o ' \b(void|int|char|float|double|long|short|signed|unsigned|bool|wchar_t|std::vector|std::string|std::map|std::set|std::unordered_map|std::unordered_set|std::tuple|std::pair|std::list|std::deque|std::stack|std::queue|std::shared_ptr|std::unique_ptr|std::weak_ptr|std::function|std::optional|std::variant|std::array|std::initializer_list|std::regex|std::chrono::duration|std::chrono::time_point|std::wstring)\b [a-zA-Z0-9_]+\(' "$find")
+	gasit=$(egrep -o ' \b(void|int|char|float|double|long|short|signed|unsigned|bool|wchar_t|std::vector|std::string|std::map|std::set|std::unordered_map|std::unordered_set|std::tuple|std::pair|std::list|std::deque|std::stack|std::queue|std::shared_ptr|std::unique_ptr|std::weak_ptr|std::function|std::optional|std::variant|std::array|std::initializer_list|std::regex|std::chrono::duration|std::chrono::time_point|std::wstring)\b [a-zA-Z0-9_]+\(' "$find"|tr ' ' '\n')
 	echo $gasit
 done
